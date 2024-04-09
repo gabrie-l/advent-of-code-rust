81f7eb5ad0f8b7a282fa46b93d6943f2b4bd0a56
gabrie-l <gabrielmidar@gmail.com>
2024-04-09 13:08:28 -0700

    day7 part2 solved

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -35,43 +35,44 @@ struct Hand {
     hand: Vec<char>,
     rank: u32,
     bid: u32,
+    handtype: HandType,
 }
 
 impl Hand {
-    fn new(map: HashMap<char, u32>, htype: HandType, bid: u32, hand: Vec<char>) -> Self {
-        let mut max_val: u32 = 0;
-        let mut max_count: u8 = 1;
-        // for ch in map.keys() {
-        //     if htype == HandType::HighCard {
-        //         if card_val > max_val {
-        //             max_val = card_val;
-        //         }
-        //         continue;
-        //     }
-        //     let count: u8 = *map.get(ch).expect("Key error") as u8;
-        //     if count > 1 && count >= max_count {
-        //         max_count = count;
-        //         if card_val > max_val {
-        //             max_val = card_val
-        //         }
-        //     }
-        // }
+    fn new(htype: HandType, bid: u32, hand: Vec<char>) -> Self {
         let rank = htype.value();
-        Hand { hand, rank, bid }
+        Hand {
+            hand,
+            rank,
+            bid,
+            handtype: htype,
+        }
     }
 }
 
 fn card_value(card: char) -> u32 {
     match card {
+        'J' => 1, //modified for part 2
         '2'..='9' => card.to_digit(10).unwrap(),
         'T' => 10,
-        'J' => 20,
         'Q' => 30,
         'K' => 40,
         'A' => 50,
         _ => 0,
     }
 }
+fn match_hand(distinct_values: u32, max_count: u32) -> HandType {
+    match (distinct_values, max_count) {
+        (1, 5) => HandType::FiveOfAKind,
+        (2, 4) => HandType::FourOfAKind,
+        (2, 3) => HandType::FullHouse,
+        (3, 3) => HandType::ThreeOfAKind,
+        (3, 2) => HandType::TwoPair,
+        (4, 2) => HandType::OnePair,
+        _ => HandType::HighCard,
+    }
+}
+
 fn main() {
     let f = File::open("input").expect("File not found");
     let reader = BufReader::new(f);
@@ -85,27 +86,38 @@ fn main() {
         let mut card_counts: HashMap<char, u32> = HashMap::new();
         let hand = line.split_whitespace().nth(0).unwrap();
         let bid: u32 = line.split_whitespace().nth(1).unwrap().parse().unwrap();
+        let mut joker = false;
         for card in hand.chars() {
+            if card == 'J' {
+                joker = true
+            }
             *card_counts.entry(card).or_insert(0) += 1;
         }
-        let distinct_values = card_counts.keys().count() as u32;
-        let max_count = card_counts.values().cloned().max().unwrap_or(0);
 
-        let hand_type = match (distinct_values, max_count) {
-            (1, 5) => HandType::FiveOfAKind,
-            (2, 4) => HandType::FourOfAKind,
-            (2, 3) => HandType::FullHouse,
-            (3, 3) => HandType::ThreeOfAKind,
-            (3, 2) => HandType::TwoPair,
-            (4, 2) => HandType::OnePair,
-            _ => HandType::HighCard,
+        let (distinct_values, max_count) = (
+            card_counts.keys().count() as u32,
+            card_counts.values().cloned().max().unwrap_or(1),
+        );
+
+        let hand_type = match joker {
+            true => {
+                let num_jokers = card_counts.remove(&'J').unwrap();
+                let joker_hand = (
+                    card_counts.keys().count() as u32,
+                    card_counts.values().cloned().max().unwrap_or(0) + num_jokers,
+                );
+                let joker_cards = match_hand(joker_hand.0, joker_hand.1);
+                let std_cards = match_hand(distinct_values, max_count);
+                if joker_cards.value() > std_cards.value() {
+                    joker_cards
+                } else {
+                    std_cards
+                }
+            }
+            false => match_hand(distinct_values, max_count),
         };
-        hands.push(Hand::new(
-            card_counts,
-            hand_type,
-            bid,
-            hand.chars().collect(),
-        ));
+        println!("decision {:?}", hand_type);
+        hands.push(Hand::new(hand_type, bid, hand.chars().collect()));
     }
     hands.sort_by(|a, b| {
         // Compare ranks first
@@ -114,21 +126,23 @@ fn main() {
             return rank_cmp;
         }
 
-        // Implement tiebreakers here
-        // For example, if rank is equal, compare bids
         for (a_c, b_c) in a.hand.iter().zip(&b.hand) {
             let rank_cmp = card_value(*a_c).cmp(&card_value(*b_c));
             if rank_cmp != Equal {
                 return rank_cmp;
             }
-            // 'a_c' represents an element from 'a.hand' and 'b_c' represents the corresponding element from 'b.hand'
-            // You can perform operations on 'a_c' and 'b_c' here
         }
         a.bid.cmp(&b.bid)
     });
     let mut total = 0u32;
     for (idx, h) in hands.into_iter().enumerate() {
-        println!("hand {:?} rank {} bid {}", h.hand, idx + 1, h.bid);
+        println!(
+            "hand {:?} rank {} bid {} type {:?}",
+            h.hand,
+            idx + 1,
+            h.bid,
+            h.handtype
+        );
         total += (1 + idx as u32) * h.bid;
     }
     println!("{}", total);
